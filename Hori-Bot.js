78e3216018a0350d7d1702a0b7a2394379c5589d
Sachu <93819264+Sachu-Settan@users.noreply.github.com>
2022-05-27 07:56:54 +0530

    Update Hori-Bot.js

--- Hori-Bot.js
+++ Hori-Bot.js
@@ -1,6 +1,7 @@
 /*
 * MODULES
 */
+
 require('./config')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
